32b58ec15426102f6e03e7d231376d6ce89c73d7
DenilsonDesigns <daniellanguilller@gmail.com>
2018-09-25 23:28:11 +1000

    changed package.json

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 //ROUTES
 const users = require("./routes/api/users");
@@ -39,6 +40,16 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+
+//SERVE STATIC ASSETS IF IN PRODUCTION
+if (process.env.NODE_ENV === "production") {
+  //SET STATIC FOLDER
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 //PORT CONNECTION
 const port = process.env.PORT || 5000;
