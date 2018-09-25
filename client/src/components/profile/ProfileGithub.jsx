2435b7e76005134b8474cb246e9e1bb06f1060e0
DenilsonDesigns <daniellanguilller@gmail.com>
2018-09-25 23:41:20 +1000

    small change

--- client/src/components/profile/ProfileGithub.jsx
+++ client/src/components/profile/ProfileGithub.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
-import githubKeys from "../../../src/config/config";
+// import githubKeys from "../../../src/config/config";
 
 class ProfileGithub extends Component {
   constructor(props) {
